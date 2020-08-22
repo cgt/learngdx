877bbed24ddb6d5d47f85ed14a1db428bacb2721
Christoffer G. Thomsen <chris@cgt.name>
2020-08-22 14:26:25 +0200

    Convert Main to Kotlin

--- core/src/main/java/name/cgt/learngdx/Main.java
+++
@@ -1,7 +0,0 @@
-package name.cgt.learngdx;
-
-import com.badlogic.gdx.ApplicationAdapter;
-
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-}
---
+++ core/src/main/kotlin/name/cgt/learngdx/Main.kt
@@ -0,0 +1,6 @@
+package name.cgt.learngdx
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms.  */
+class Main : ApplicationAdapter()
