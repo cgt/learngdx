e99023b70cece225bc78baef9ea8313a86d3fa32
Christoffer G. Thomsen <chris@cgt.name>
2020-08-23 15:15:17 +0200

    Remove bucket drawing code

--- core/src/main/kotlin/name/cgt/learngdx/Main.kt
+++ core/src/main/kotlin/name/cgt/learngdx/Main.kt
@@ -4,31 +4,15 @@ import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
-import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.SpriteBatch
-import com.badlogic.gdx.math.Rectangle
 
 class Main : ApplicationAdapter() {
 
-    private lateinit var bucketImage: Texture
     private lateinit var camera: OrthographicCamera
-    private lateinit var batch: SpriteBatch
-    private lateinit var bucket: Rectangle
 
     override fun create() {
-        bucketImage = Texture(Gdx.files.internal("bucket.png"))
-
         camera = OrthographicCamera()
         camera.setToOrtho(false, 640f, 480f)
 
-        batch = SpriteBatch()
-
-        bucket = Rectangle().apply {
-            x = 0f
-            y = 0f
-            width = 80f
-            height = 80f
-        }
     }
 
     override fun render() {
@@ -37,16 +21,5 @@ class Main : ApplicationAdapter() {
             glClear(GL20.GL_COLOR_BUFFER_BIT)
         }
         camera.update()
-        batch.apply {
-            projectionMatrix = camera.combined
-            begin()
-            draw(bucketImage, bucket.x, bucket.y)
-            end()
-        }
-    }
-
-    override fun dispose() {
-        bucketImage.dispose()
-        batch.dispose()
     }
 }
