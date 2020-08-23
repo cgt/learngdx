186651ff693a4f56a2a643bc729502c371c05d29
Christoffer G. Thomsen <chris@cgt.name>
2020-08-23 16:32:18 +0200

    Fix bot paddle movement, add moving ball

--- core/src/main/kotlin/name/cgt/learngdx/Main.kt
+++ core/src/main/kotlin/name/cgt/learngdx/Main.kt
@@ -11,8 +11,8 @@ import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType
 import com.badlogic.gdx.math.Rectangle
 
-private const val width = 640f
-private const val height = 480f
+private const val WIDTH = 640f
+private const val HEIGHT = 480f
 
 class Main : ApplicationAdapter() {
 
@@ -21,15 +21,17 @@ class Main : ApplicationAdapter() {
 
     override fun create() {
         camera = OrthographicCamera()
-        camera.setToOrtho(false, width, height)
+        camera.setToOrtho(false, WIDTH, HEIGHT)
         shape = ShapeRenderer()
 
         paddle1 = Paddle(shape, camera)
-        paddle2 = Paddle(shape, camera, startPosition = Pair(width - 10, height - 40))
+        paddle2 = Paddle(shape, camera, startPosition = Pair(WIDTH - 10, HEIGHT - 40))
+        ball = Ball(shape, camera)
     }
 
     private lateinit var paddle1: Paddle
     private lateinit var paddle2: Paddle
+    private lateinit var ball: Ball
 
     override fun render() {
         Gdx.gl.apply {
@@ -44,8 +46,37 @@ class Main : ApplicationAdapter() {
         }
 
         paddle2.patrol()
+        ball.move()
+
         paddle1.render()
         paddle2.render()
+        ball.render()
+    }
+}
+
+class Ball(
+    private val shape: ShapeRenderer,
+    private val camera: Camera
+) {
+    private val box = Rectangle().apply {
+        x = WIDTH / 2
+        y = HEIGHT / 2
+        width = 8f
+        height = 8f
+    }
+
+    private var direction: Direction = Direction.LEFT
+
+    fun move() {
+        box.x += 1
+    }
+
+    fun render() {
+        shape.projectionMatrix = camera.combined
+        shape.begin(ShapeType.Filled)
+        shape.color = Color.WHITE
+        shape.rect(box.x, box.y, box.width, box.height)
+        shape.end()
     }
 }
 
@@ -63,15 +94,15 @@ class Paddle(
     private var direction: Direction = Direction.UP
 
     fun patrol() {
-        if (box.y + 1 > height - 40) {
-            direction = Direction.DOWN
-        } else if (box.y - 1 < 0) {
-            direction = Direction.UP
-        }
         if (direction == Direction.UP) {
             moveUp(false)
         } else if (direction == Direction.DOWN) {
             moveDown(false)
+        }
+        if (direction == Direction.UP && box.y + 1 >= HEIGHT - 40) {
+            direction = Direction.DOWN
+        } else if (direction == Direction.DOWN && box.y - 1 <= 0) {
+            direction = Direction.UP
         }
     }
 
@@ -84,7 +115,7 @@ class Paddle(
     }
 
     fun moveUp(sprint: Boolean) {
-        if (box.y + 1 < height - 40) {
+        if (box.y + 1 < HEIGHT - 40) {
             box.y += 1
             if (sprint) {
                 moveUp(sprint = false)
@@ -104,5 +135,7 @@ class Paddle(
 
 enum class Direction {
     UP,
-    DOWN
+    DOWN,
+    LEFT,
+    RIGHT,
 }
